56c26f1be55162b08656b4602365de05228f037a
choiwanmin <choi.wm512@gmail.com>
2024-08-13 16:28:32 +0900

    refactor:application.properties를 활용한 ip 주소 변경 적용 및 cors 에러 수정

--- kosta_finalpjt_restapi/src/main/java/com/example/demo/oracledb/auth/JwtAuthenticationFilter.java
+++ kosta_finalpjt_restapi/src/main/java/com/example/demo/oracledb/auth/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import jakarta.servlet.FilterChain;
@@ -17,46 +18,57 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
-	
+
 	@Value("${allowed.ip.frontaddress}")
 	private String ipfrontaddress;
-	
+
 	@Value("${allowed.dns.address}")
 	private String dnsaddress;
-	
-    private final MyTokenProvider myTokenProvider;
-    private final List<String> allowedOrigins = Arrays.asList(ipfrontaddress, dnsaddress);
 
-    @Override // 필터가 할일 구현
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        HttpServletRequest req = (HttpServletRequest) request;
-        String origin = req.getHeader("Origin");
-        if (allowedOrigins.contains(origin)) {
-            res.setHeader("Access-Control-Allow-Origin", origin);
-        } else {
-            res.setHeader("Access-Control-Allow-Origin", "");
-        };
-        res.setHeader("Vary", "Origin");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-        res.setHeader("Access-Control-Allow-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	private final MyTokenProvider myTokenProvider;
 
-        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-            res.setStatus(HttpServletResponse.SC_OK);
-            return;
-        }
+//	private final List<String> allowedOrigins = Arrays.asList(ipfrontaddress, dnsaddress);
+//	private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000");
 
-        String token = myTokenProvider.resolveToken((HttpServletRequest) request);
-        if (token != null && myTokenProvider.validateToken(token)) {
-            Authentication authentication = myTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-        chain.doFilter(request, response);
-    }
+	@Override // 필터가 할일 구현
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		System.out.println("=========ipfrontaddress:" + ipfrontaddress);
+		System.out.println("=========dnsaddress:" + dnsaddress);
+
+		List<String> allowedOrigins = Arrays.asList(ipfrontaddress, dnsaddress);
+
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		String origin = req.getHeader("Origin");
+
+		if (allowedOrigins.contains(origin)) {
+			res.setHeader("Access-Control-Allow-Origin", origin);
+		} else {
+			res.setHeader("Access-Control-Allow-Origin", "");
+		}
+		;
+		res.setHeader("Vary", "Origin");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+		res.setHeader("Access-Control-Allow-Max-Age", "3600");
+		res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+		if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+			res.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
+
+		String token = myTokenProvider.resolveToken((HttpServletRequest) request);
+		if (token != null && myTokenProvider.validateToken(token)) {
+			Authentication authentication = myTokenProvider.getAuthentication(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		chain.doFilter(request, response);
+	}
 
 }
